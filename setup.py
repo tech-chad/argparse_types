ca43e8a294b0ecc62b802a768f3c00dd9057ea1b
Chad Larson <techchad2@gmail.com>
2020-05-16 15:24:15 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup()
+"""
+setup(
+    name='argparse_types',
+    version='0.0.0',
+    url='',
+    license='',
+    author='chad',
+    author_email='',
+    description='extra argparse types'
+)
+"""
