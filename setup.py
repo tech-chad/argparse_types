86059c7bd3b11e707379b2f0479dc38d24e85047
Chad Larson <techchad2@gmail.com>
2020-05-28 22:24:27 -0500

    updated setup and git ignore files

--- setup.py
+++ setup.py
@@ -1,14 +1,2 @@
 from setuptools import setup
 setup()
-"""
-setup(
-    name='argparse_types',
-    version='0.0.0',
-    url='',
-    license='',
-    author='chad',
-    author_email='',
-    description='extra argparse types'
-    install_requires=['pytest']
-)
-"""
